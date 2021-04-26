c16f21bd2b0de107e4b072c58b60044134720684
Sergio <sbouden16@gmail.com>
2021-04-26 17:01:07 +0200

    Add newsfeed_opinion to show and opinion.rb

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -2,6 +2,6 @@ class Opinion < ApplicationRecord
   belongs_to :author, class_name: 'User'
   validates :text, presence: true, length: { maximum: 260 }
   has_many :likes, dependent: :destroy
-  scope :ordered_by_most_recent, -> { includes([:author]).order(created_at: :desc) }
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
   scope :newsfeed_opinions, -> { includes(:author).ordered_by_most_recent }
 end
